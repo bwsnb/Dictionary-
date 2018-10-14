f9aebb6449becdcb31e04f2d3ba4f469bf5a8e5f
bwsnb <bwsnbb@163.com>
2018-10-14 16:25:18 +0800

    完成

---
+++ Dictionary泛型集合之基本使用/PhoneController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary泛型集合之基本使用
+{
+    /// <summary>
+    /// C.控制器层
+    /// </summary>
+    class PhoneController
+    {
+        //
+        Dictionary<string, string> dic = new Dictionary<string, string>();
+
+        
+        /// <summary>
+        /// 增加电话号码
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="number"></param>
+        public void AddNumber(string name,string number)
+        {
+            if (dic.ContainsKey(name))
+            {
+                Console.WriteLine("已存在用户");
+            }
+            else
+            {
+                dic.Add(name, number);
+            }
+            
+
+        }
+
+        public void RemoveNumber(string name)
+        {
+            dic.Remove(name);
+        }
+
+        public void ChangeNumber(string name, string number)
+        {
+            dic[name] = number;
+        }
+
+        public void ShowAllNumber()
+        {
+            foreach (var item in dic.Keys)
+            {
+                Console.WriteLine($"姓名：{item}，电话号码：{dic[item]}");
+            }
+        }
+
+    }
+}
--- Dictionary泛型集合之基本使用/Program.cs
+++ Dictionary泛型集合之基本使用/Program.cs
@@ -10,6 +10,51 @@ namespace Dictionary泛型集合之基本使用
     {
         static void Main(string[] args)
         {
+
+            PhoneController pc = new PhoneController();
+
+            Console.WriteLine("***************************\n**********电话簿***********\n***************************\n");
+            while (true)
+            {
+                
+                Console.WriteLine("1.添加号码\n2.删除号码\n3.修改号码\n4.查询所有号码");
+                int num = int.Parse(Console.ReadLine());
+
+
+                switch (num)
+                {
+                    case 1:
+                        Console.Write("请输入姓名：");
+                        string name = Console.ReadLine();
+                        Console.WriteLine();
+                        Console.Write("请输入电话号码：");
+                        string number = Console.ReadLine();
+                        Console.WriteLine();
+                        pc.AddNumber(name, number);
+                        break;
+                    case 2:
+                        pc.ShowAllNumber();
+                        Console.Write("请输入要删除号码的姓名：");
+                        string Rname = Console.ReadLine();
+                        pc.RemoveNumber(Rname);
+                        break;
+                    case 3:
+                        pc.ShowAllNumber();
+                        Console.WriteLine("请输入要修改号码的姓名：");
+                        string Cname = Console.ReadLine();
+                        pc.RemoveNumber(Cname);
+                        break;
+
+                    case 4:
+                        pc.ShowAllNumber();
+                        break;
+
+                }
+            }
+
+
+            Console.ReadKey();
+
         }
     }
 }
